673f0ee0dc402c4f1dc0140065a128fc23b1a24e
은혜 <102123860+eunhye0127@users.noreply.github.com>
2023-03-23 01:01:48 +0900

    Add files via upload

---
+++ 02/components/Header.js
@@ -0,0 +1,12 @@
+export default function Header() {
+    return (
+      <header>
+        <nav>
+          <img src="./Group.png" className="nav--icon"></img>
+          <h3 className="nav--logo_text">ReactFacts</h3>
+          <h4 className="nav--title">React Course - Project 1</h4>
+        </nav>
+      </header>
+    );
+  }
+  
